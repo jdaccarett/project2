9935ced8e5e7c3e8c20167fbabeba74a42e16557
jdaccarett <jdaccarett101@gmail.com>
2017-05-02 10:05:22 -0700

    Update testalgos.py

--- testalgos.py
+++ testalgos.py
@@ -1,11 +1,15 @@
 # This file will test all four functions using the same test cases
 
-#import 2nd algorithm
+#import 1st algorithm
+from algo1 import changeslowHelper as getchange1
+
+#import Juans's 2nd algorithm
+from algo2 import changegreedy as getchange2
 
 #import 3rd algorithm
+from algo3 import changeDP as getchange3
 
-# import Juans's 2nd algorithm
-from algo2 import changegreedy as getchange2
+
 
 
 def main():
@@ -32,68 +36,95 @@ def main():
     questhree_Testcase.append(V3)
 
 
-    testCase = []
-    testCase.append(testOne)
-    testCase.append(testTwo)
-    testCase.append(testThree)
+    #testCase = []
+    #testCase.append(testOne)
+    #testCase.append(testTwo)
+    #testCase.append(testThree)
 
 
     coinAmount = [15, 29, 31]
 
 
-    for x in range(1, 2):
+    for x in range(1, 4):
         algoID = x
 
         if (algoID == 1):
-            getchange = getchange2
+            getchange = getchange3
 
         elif (algoID == 2):
-            print("algoID",algoID)
             getchange = getchange2
 
-        else:
-            print(algoID)
+
+        elif (algoID == 3):
+            getchange = getchange1
 
         # Loop through test cases
-        for testinput in range(0, len(testCase)):
-            cArray, m = getchange(testCase[testinput], coinAmount[testinput])
-            print("Array", cArray)
-            print("m", m)
+        #for x in range(0, len(testCase)):
+         #   print("ALGO ID:", algoID)
+          #  cArray, m = getchange(testCase[x], coinAmount[x])
+           # print(cArray)
+            #print(m)
 
 
-        # Loop for each integer value of A in [1, 2, 3, …, 50] in test cases V1, V2 and V3
+        # Loop for each integer value of A in [1, 2, 3, …, 50] in (test case V1)
+        print("ALGO ID:", algoID, "TESTCASE V1,V2,V3  A[1,2,3..50]")
+        print("____________________________________________ ")
         for amount in range(1, 51):
-            #Values of V1
-            cArray, m = getchange(questhree_Testcase[0], amount)
-            print("AMOUNT of ", amount, "|" "ARRAY = ", cArray)
-            print("Number of Coins = ", m)
-            # Values of V2
-            cArray, m = getchange(questhree_Testcase[1], amount)
-            print("AMOUNT of ", amount, "|" "ARRAY = ", cArray)
-            print("Number of Coins = ", m)
-            # Values of V3
-            cArray, m = getchange(questhree_Testcase[2], amount)
-            print("AMOUNT of ", amount, "|" "ARRAY = ", cArray)
-            print("Number of Coins = ", m)
+            if algoID > 0:
+                # Values of V1
+                cArray, m = getchange(questhree_Testcase[0], amount)
+                print(cArray)
+                print("AlgoId",algoID, "Number of Coins = ", m)
+
+        # Loop for each integer value of A in [1, 2, 3, …, 50] in (test case V2)
+        print("ALGO ID:", algoID, "TESTCASE V1,V2,V3  A[1,2,3..50]")
+        print("____________________________________________ ")
+        for amount in range(1, 51):
+            if algoID > 0:
+                # Values of V2
+                cArray, m = getchange(questhree_Testcase[1], amount)
+                print(cArray)
+                print("AlgoId",algoID, "Number of Coins = ", m)
+
+        # Loop for each integer value of A in [1, 2, 3, …, 50] in (test case V3)
+        print("ALGO ID:", algoID, "TESTCASE V1,V2,V3  A[1,2,3..50]")
+        print("____________________________________________ ")
+        for amount in range(1, 51):
+            if algoID > 0:
+                # Values of V2
+                cArray, m = getchange(questhree_Testcase[2], amount)
+                print(cArray)
+                print("AlgoId",algoID, "Number of Coins = ", m)
 
 
 
         # For each integer value of A in [2000, 2001, 2002, …, 2200]
-        # determine the number of coins that
-        # changegreedy and changedp requires for each denomination set
+        print("ALGO ID:", algoID, "TESTCASE V1,V2,V3  A[2000,2001,2002,…2200]")
+        print("____________________________________________")
+        #V1 case
         for amount in range(2000, 2200):
             #Values of V1
             cArray, m = getchange(questhree_Testcase[0], amount)
-            print("AMOUNT of ", amount, "|" "ARRAY = ", cArray)
-            print("Number of Coins = ", m)
+            print(cArray)
+            print("AlgoId", algoID, "Number of Coins = ", m)
+
+        print("ALGO ID:", algoID, "TESTCASE V1,V2,V3  A[2000,2001,2002,…2200]")
+        print("____________________________________________")
+        #V2 case
+        for amount in range(2000, 2200):
             # Values of V2
             cArray, m = getchange(questhree_Testcase[1], amount)
-            print("AMOUNT of ", amount, "|" "ARRAY = ", cArray)
-            print("Number of Coins = ", m)
+            print(cArray)
+            print("AlgoId", algoID, "Number of Coins = ", m)
+
+        print("ALGO ID:", algoID, "TESTCASE V1,V2,V3  A[2000,2001,2002,…2200]")
+        print("____________________________________________")
+        #V3 Case
+        for amount in range(2000, 2200):
             # Values of V3
             cArray, m = getchange(questhree_Testcase[2], amount)
-            print("AMOUNT of ", amount, "|" "ARRAY = ", cArray)
-            print("Number of Coins = ", m)
+            print(cArray)
+            print("AlgoId", algoID, "Number of Coins = ", m)
 
 
 if __name__ == "__main__":
